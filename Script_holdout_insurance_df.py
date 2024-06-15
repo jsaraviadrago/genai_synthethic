03249a8452836c89edb71be5495f78bcf85f47d3
JC Saravia <jsaraviadrago@gmail.com>
2024-06-15 09:02:52 -0500

    Editing Notebook_task1.ipynb

--- Script_holdout_insurance_df.py
+++ Script_holdout_insurance_df.py
@@ -1,6 +1,19 @@
 import pandas as pd
 
-raw_insurance_compare_update = url2 + insurance_compare
-url2 = "https://raw.githubusercontent.com/jsaraviadrago/genai_synthethic/main/"
-
 insurance_compare = 'insurance_compare.csv'
+url = "https://raw.githubusercontent.com/jsaraviadrago/genai_synthethic/main/"
+
+raw_insurance_compare_update = url + insurance_compare
+
+df_insurance_compare_update = pd.read_csv(raw_insurance_compare_update) #step 3
+
+# Filter the data just to get real data.
+df_insurance_compare_update_real = df_insurance_compare_update.loc[df_insurance_compare_update['Data'] == 'Real']
+
+# Hold 50% of the real data to make the synthetise data.
+df_insurance_compare_update_real_train = df_insurance_compare_update_real.sample(frac=0.5).reset_index()
+
+#df_insurance_compare_update_real_train.to_csv(file_name, sep=',', encoding='utf-8')
+
+
+
