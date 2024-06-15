856b19be1c034fafd16e1c2eb184ee11d6dc0628
JC Saravia <jsaraviadrago@gmail.com>
2024-06-15 09:09:46 -0500

    Editing Script_holdout_insurance_df.py

--- Script_holdout_insurance_df.py
+++ Script_holdout_insurance_df.py
@@ -11,9 +11,11 @@ df_insurance_compare_update = pd.read_csv(raw_insurance_compare_update) #step 3
 df_insurance_compare_update_real = df_insurance_compare_update.loc[df_insurance_compare_update['Data'] == 'Real']
 
 # Hold 50% of the real data to make the synthetise data.
-df_insurance_compare_update_real_train = df_insurance_compare_update_real.sample(frac=0.5).reset_index()
+#df_insurance_compare_update_real_train = df_insurance_compare_update_real.sample(frac=0.5).reset_index()
+df_insurance_compare_update_real_train = df_insurance_compare_update_real.iloc[:669,] # Getting the first 668
+df_insurance_compare_update_real_test = df_insurance_compare_update_real.iloc[670:,] # Getting the last 668
 
-#df_insurance_compare_update_real_train.to_csv(file_name, sep=',', encoding='utf-8')
-
+#df_insurance_compare_update_real_train.to_csv(insurance_compare_train.csv, sep=',', encoding='utf-8')
+#df_insurance_compare_update_real_test.to_csv(insurance_compare_test.csv, sep=',', encoding='utf-8')
 
 
