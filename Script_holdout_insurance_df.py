c7f822c9f73238aa2b5ca7733ace5810825b10fd
JC Saravia <jsaraviadrago@gmail.com>
2024-06-15 08:52:02 -0500

    Editing Notebook_task1.ipynb

---
+++ Script_holdout_insurance_df.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+raw_insurance_compare_update = url2 + insurance_compare
+url2 = "https://raw.githubusercontent.com/jsaraviadrago/genai_synthethic/main/"
+
+insurance_compare = 'insurance_compare.csv'
